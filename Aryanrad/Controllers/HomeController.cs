3a73dfec6d0fc5b0f6ad861a9e8446f7b2a80ee2
r <r@DESKTOP-2PJL3H7>
2023-01-12 17:25:17 +0330

    Push git hun

---
+++ Aryanrad/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Aryanrad.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aryanrad.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Ruls()
+        {
+            return View();
+        }
+
+        
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails2()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails3()
+        {
+            return View();
+        }
+
+        public IActionResult Category()
+        {
+            return View();
+        }
+
+        public IActionResult Category2()
+        {
+            return View();
+        }
+
+        public IActionResult Category3()
+        {
+            return View();
+        }
+
+        public IActionResult Category4()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
